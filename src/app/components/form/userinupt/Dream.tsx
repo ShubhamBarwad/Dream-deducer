82c4cb30c80231518d1ee5eeb5f50b2db835eba6
shubham <sbarwad20595@gmail.com>
2024-03-30 19:02:02 +0530

    Feature/DD-002: Display dream analysis on frontend

--- src/app/components/form/userinupt/Dream.tsx
+++ src/app/components/form/userinupt/Dream.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+'use client'
+import React, { useRef, useState } from 'react';
+import model from '@/app/geminiAi/geminiAi';
 import { analyseDream } from '@/app/lib/action/AnalyseDream';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-async function Dream() {
-    let analysis;
-    console.log('analysis', await analysis);
+function Dream() {
+    const genAi = new GoogleGenerativeAI('AIzaSyCVgJkeGVjDVrI61EVHpQqiScx3fZNR3mw');
+    const model = genAi.getGenerativeModel({ model: "gemini-pro" });
+    const [analysis, setAnalysis] = useState([]);
+    const refDream = useRef(null);
+    const radio = useRef(null);
+
+    async function analyseDream(dreamString: string) {
+
+        console.log('analysing dream plase wait');
+        const bodyContent = generateBodyContent(dreamString);
+
+        const result = await model.generateContent(bodyContent);
+        const analysisString = refactorString(result.response.text());
+        console.log(analysisString);
+        setAnalysis(analysisString);
+
+    }
+
+    const generateBodyContent = (dreamString: string) => {
+        return 'Hi, please analyse my dream: ' + dreamString;
+    }
+
+    const refactorString = (string: string) => {
+        return string.split('**');
+    }
+
     return (
         <>
-            <form className='max-w-96 mx-auto flex flex-col gap-5 my-3' action={analysis = await analyseDream}>
+            <div className='max-w-96 mx-auto flex flex-col gap-5 my-3'>
                 <label className='text required'>
                     <span>Tell me about your dream</span>
-                    <textarea className='form-input' name='dream' placeholder="Tell me about your dream..." rows={5} required />
+                    <textarea className='form-input' name='dream' placeholder="Tell me about your dream..." rows={5} required ref={refDream} />
                 </label>
                 <div className='flex flex-row gap-4'>
                     <label className='checkbox'>
@@ -21,11 +48,15 @@ async function Dream() {
                         <span>Didn't sleep well</span>
                     </label>
                 </div>
-                <button className='btn btn-primary' type="submit">Find what my dreams mean</button>
-            </form>
-            {/* {
-                analysis && <pre>{JSON.stringify(analysis, null, 2)}</pre>
-            } */}
+                <button className='btn btn-primary' onClick={() => analyseDream(refDream.current?.value)}>Find what my dreams mean</button>
+            </div>
+            {
+                analysis.length && (
+                    <div className='mx-auto flex flex-col gap-5 my-3'>
+                        {analysis.map((item) => <p>{item}</p>)}
+                    </div>
+                )
+            }
         </>
     )
 }
